e015527d478254701c7d44e4ccee9cc8c1cb8a61
Maksudul Rana <ranama2@vcu.edu>
2024-04-25 03:00:55 -0400

    change1

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myfirstapplication
 
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
@@ -28,6 +29,27 @@ class MainActivity : AppCompatActivity() {
             val submittedPassword = password.text.toString()
 
             // TODO: Create the logic to verify user login details, EMAIL should be valid, Password must have 8 length including small/capital letters with digit and special characters
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(submittedEmail).matches())
+            {
+                println("Invalid Email Address")
+            }
+
+            if (submittedPassword.length < 8) {
+                println("Minimum 8 characters")
+            } else if (!submittedPassword.matches(".*[A-Z].*".toRegex())){
+                println("Must contain at least 1 uppercase character")
+            } else if (!submittedPassword.matches(".*[a-z].*".toRegex())){
+                println("Must contain at least 1 lowercase character")
+            } else if (!submittedPassword.matches(".*[@#\$%^&+=].*".toRegex())){
+                println("Must contain at least 1 special character")
+            }
+
+
+
+
+
+
         }
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -36,5 +58,6 @@ class MainActivity : AppCompatActivity() {
 //            insets
 //        }
     }
+
 
 }
